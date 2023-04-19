26c965c5f338442f71a859effda53e2f7071bd24
Diogo Zanetti <diogozanetti28@gmail.com>
2023-04-19 19:39:55 -0300

    Adicionar arquivos de projeto.

---
+++ dias_eMm-idade/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace dias_eMm_idade
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtano = new System.Windows.Forms.TextBox();
+            this.txtmes = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtdia = new System.Windows.Forms.TextBox();
+            this.txtresultado = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtano
+            // 
+            this.txtano.Location = new System.Drawing.Point(21, 58);
+            this.txtano.Name = "txtano";
+            this.txtano.Size = new System.Drawing.Size(100, 20);
+            this.txtano.TabIndex = 0;
+            // 
+            // txtmes
+            // 
+            this.txtmes.Location = new System.Drawing.Point(21, 97);
+            this.txtmes.Name = "txtmes";
+            this.txtmes.Size = new System.Drawing.Size(100, 20);
+            this.txtmes.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "ANOS";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(18, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "MESES";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(21, 198);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtdia
+            // 
+            this.txtdia.Location = new System.Drawing.Point(21, 134);
+            this.txtdia.Name = "txtdia";
+            this.txtdia.Size = new System.Drawing.Size(100, 20);
+            this.txtdia.TabIndex = 4;
+            // 
+            // txtresultado
+            // 
+            this.txtresultado.AutoSize = true;
+            this.txtresultado.Location = new System.Drawing.Point(31, 168);
+            this.txtresultado.Name = "txtresultado";
+            this.txtresultado.Size = new System.Drawing.Size(0, 13);
+            this.txtresultado.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(18, 120);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(28, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Dias";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(180, 233);
+            this.Controls.Add(this.txtresultado);
+            this.Controls.Add(this.txtdia);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtmes);
+            this.Controls.Add(this.txtano);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtano;
+        private System.Windows.Forms.TextBox txtmes;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtdia;
+        private System.Windows.Forms.Label txtresultado;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ dias_eMm-idade/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace dias_eMm_idade
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int anos, meses, dias, idadeEmDias;
+
+            anos = int.Parse(txtano.Text);
+            meses = int.Parse(txtmes.Text);
+            dias = int.Parse(txtdia.Text);
+
+
+
+            idadeEmDias = (anos * 365) + (meses * 30) + dias;
+
+
+
+            txtresultado.Text = idadeEmDias.ToString();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
